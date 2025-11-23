012f794da8128d5434ff8d725243ff4d2393faca
Tu Nombre <rubenmarti@MacBook-Air-de-Ruben.local>
2025-11-23 13:13:27 -0500

    Migra a postgres

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -31,7 +31,7 @@ const databaseConfig = ({ env }: ConfigParams): DatabaseConfig => {
       connection: {
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
+        database: env('DATABASE_NAME', 'facopec_strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
         schema: env('DATABASE_SCHEMA', 'public'),
